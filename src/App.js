d19782cea9abb8ce14b44d2e087ac888eb20615d
Sam Swerczek <sam_swerczek@msn.com>
2021-10-30 11:08:38 -0500

    Fixing page title and icon. Fixing tests.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
       <header className="App-header">
         <div>
           <h1>Hello World</h1>
-          <p>This site is under construction. In the meantime, please reach out to me at <a class="App-link" href="mailto:sam_swerczek@msn.com">sam_swerczek@msn.com</a>.</p>
+          <p>This site is under construction. In the meantime, please reach out to me at <a className="App-link" href="mailto:sam_swerczek@msn.com">sam_swerczek@msn.com</a>.</p>
         </div>
       </header>
     </div>

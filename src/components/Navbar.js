9c26e80f90477855e24d48734b50ef495011d0c2
Sam Swerczek <Sam.Swerczek@buildertrend.com>
2024-03-15 16:18:17 -0500

    fix build failure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { SidebarData } from "./Navbar/SidebarData";
-import "../../App.css";
+import { SidebarData } from "./SidebarData";
 import { IconContext } from "react-icons";
 
 function Navbar() {
